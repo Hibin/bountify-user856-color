79dc6c1d80d670ec1defdde5b74ccd06a210c963
Dmitry <dmitry@wildev.ru>
2015-03-11 10:32:47 +0300

    Fixed issue with HTML structure and getting images over http

--- color.js
+++ color.js
@@ -5,7 +5,8 @@ var read            = require( 'fs-readdir-recursive' )
     , minimatch     = require( 'minimatch' )
     , cheerio       = require( 'cheerio' )
     , ColorThief    = require( 'color-thief' )
-    , $colorThief   = new ColorThief();
+    , $colorThief   = new ColorThief()
+    , http          = require( 'http' );
 
 var $targetPath = process.argv[2];
 
@@ -15,6 +16,30 @@ var isHtml = function( $fileName ) {
 
 var toHex = function( $red, $green, $blue ) {
     return ( ( $blue | $green << 8 | $red << 16 ) | 1 << 24 ).toString( 16 ).slice( 1 );
+};
+
+var download = function( $url, $dest, $callback ) {
+    var $file    = fs.createWriteStream( $dest );
+    var $request = http.get( $url, function( $response ) {
+        $response.pipe( $file );
+
+        $file.on(
+            'finish'
+            , function() {
+                $file.close( $callback );
+            }
+        );
+
+        $file.on(
+            'error'
+            , function( $err ) {
+                fs.unlink( $dest );
+
+                if( $callback )
+                    $callback( $err.message );
+            }
+        );
+    });
 };
 
 var $files = read( $targetPath, isHtml );
@@ -27,23 +52,26 @@ $files.forEach( function( $file ) {
 
         $ = cheerio.load( $data );
 
-        var $imgSrc = $( 'div#productImage img' ).attr( 'src' );
+        var $imgSrc    = $( 'div#productImage' ).text();
+        var $imgTarget = './image-' + process.pid + '.jpg';
 
-        if( $imgSrc !== 'undefined' ) {
-            $imgSrc = $targetPath + '/' + $imgSrc;
-        }
-        
-        fs.exists( $imgSrc, function( $exists ) {
-            if( $exists ) {
-                var $color = $colorThief.getColor( $imgSrc );
-                    $color = toHex( $color[0], $color[1], $color[2] );
+        //fs.unlinkSync( $imgTarget );
 
-                fs.appendFile( $fileName, "\n" + '<div id="primary_color">#' + $color + '</div>', function( $err ) {
-                  if( $err ) throw $err;
+        download( $imgSrc, $imgTarget, function() {
+            $imgSrc = $imgTarget;
 
-                  console.log( $imgSrc + ' : ' + $color );
-                });
-            }
-        });
+            fs.exists( $imgSrc, function( $exists ) {
+                if( $exists ) {
+                    var $color = $colorThief.getColor( $imgSrc );
+                        $color = toHex( $color[0], $color[1], $color[2] );
+    
+                    fs.appendFile( $fileName, "\n" + '<div id="primary_color">#' + $color + '</div>', function( $err ) {
+                      if( $err ) throw $err;
+    
+                      console.log( $imgSrc + ' : ' + $color );
+                    });
+                }
+            });
+        } );
     });
 } );
